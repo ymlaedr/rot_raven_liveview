c104c224aa3174f060d222753e4811bf037920bc
MakotoYoshimura(YSV-Main) <ymlaedr@gmail.com>
2019-10-30 01:16:30 +0900

    add gps_record schema and mongo settings

--- liveview/priv/repo/seeds.exs
+++ liveview/priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+[
+  %{
+    module: RotRaven.Vehicle.GpsRecord,
+    collection: %{
+      :accuracy => 2777,
+      :altitude => nil,
+      :altitude_accuracy => nil,
+      :heading => nil,
+      :latitude => 33.9113796,
+      :longitude => 130.9485807,
+      :speed => nil,
+      :speed_measurement => "hybeni",
+      :timestamp => "2019-10-29T15:30:47.451Z"
+    }
+  }
+]
+|> Enum.each(&( RotRaven.Mongo.insert(&1.collection, &1.module) ))
